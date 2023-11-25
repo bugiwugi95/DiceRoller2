dc12d8e28f2608ea26d447e4e59dbf153350dd83
илья бугай <bugai.samara@yandex.ru>
2023-11-25 17:54:16 +0400

    Разминка)

--- app/src/main/java/com/hfad/diceroller2/MainActivity.kt
+++ app/src/main/java/com/hfad/diceroller2/MainActivity.kt
@@ -2,10 +2,32 @@ package com.hfad.diceroller2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hfad.diceroller2.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnRoll.setOnClickListener {
+            randomRoll()
+        }
+
+    }
+
+
+    private fun randomRoll() {
+        val drawableRandom = when (Random.nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        binding.imageRoll.setImageResource(drawableRandom)
     }
 }
